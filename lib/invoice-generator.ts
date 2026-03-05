f9947c0a70b554bc94d426e41f21cdadfa66e4d0
Subinesh21 <spidysubin46@gmail.com>
2026-03-05 13:28:17 +0530

    Fix duplicate checkout page and import issues

--- lib/invoice-generator.ts
+++ lib/invoice-generator.ts
@@ -1,6 +1,6 @@
 // Invoice Generation Service
-import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 import { INVOICE_CONFIG } from './payment-config';
 import { GSTCalculator } from './gst-calculator';
 
